4012dbaccdccaba130ae68b230219e1a2e489293
Daniel Behne <daniel.behne@mrjaffesclass.com>
2024-03-04 16:38:44 -0800

    on step 21

---
+++ TicTacToe/src/main/java/TicTacToe.java
@@ -0,0 +1,11 @@
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        TicTacToe ttt = new TicTacToe();
+    }
+    
+    public TicTacToe() {
+        View view = new View();
+        view.setVisible(true);
+    }
+}
